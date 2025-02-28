a1aa7825d9007eac8dcab7853567cb88cd42d55a
Miguel Angel <lievano.miguel.3534@eam.edu.co>
2025-02-28 16:00:04 +1300

    Commit inicial

---
+++ src/quizgitflow/QuizGitFlow.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quizgitflow;
+
+/**
+ *
+ * @author MIGUEL ANGEL
+ */
+public class QuizGitFlow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
